b9bea6efca7757e90c5b3f336403bc183c74ba1b
Kelechi Otugbuali <hilakechi@gmail.com>
2023-12-08 13:05:27 +0100

    toast notification feat DeleteModal,RenameModal,Dropzone

--- components/RenameModal.tsx
+++ components/RenameModal.tsx
@@ -14,6 +14,7 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase";
+import toast from "react-hot-toast";
 
 const RenameModal = () => {
   const { user } = useUser();
@@ -30,10 +31,16 @@ const RenameModal = () => {
   const renameFile = async () => {
     if (!user || !fileId) return;
 
+    const toastId = toast.loading('Renaming...');
+
     const docRef = doc(db, "users", user.id, "files", fileId);
     await updateDoc(docRef, {
         filename: input
     });
+    toast.success('Renamed successfully',{
+      id: toastId,
+    });
+    
     setInput('');
     setIsRenameModalOpen(false)
   };
